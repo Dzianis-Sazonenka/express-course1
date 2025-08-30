46e8b3a455c683b27655aabc6d6da67b242cfaa4
Dzianis <d.sazonenka@andersenlab.com>
2025-08-30 21:23:41 +0400

    feat: add MongoDB integration with mongoose and connection service

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
 
 const PORT = process.env.PORT || 8000;
 
@@ -7,6 +8,7 @@ const server = http.createServer(app);
 
 const {loadPlanetsData} = require('./models/planets.model')
 async function startServer() {
+    await mongoConnect();
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(`Server running at http://localhost:${PORT}/`);
